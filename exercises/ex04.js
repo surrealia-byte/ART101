59a160f50f4268b857578bbd24674b2869ab75e0
surrealia-byte <gobobobell@gmail.com>
2026-04-24 11:11:51 -0700

    himawari

--- exercises/ex04.js
+++ exercises/ex04.js
@@ -1,5 +1,13 @@
-let count=0
+
+let count=0;
+let colors=["Orchid", "Coral", "HotPink", "Plum"];
+let colorCount;
+
 $("#needy-button").click( function(){
- count= count + 1;
-   $("#needy-button").html("I was clicked " + count + " times");
+  count= count + 1;
+  colorCount=count-1
+  $("#needy-button").html("I was clicked " + count + " " + colors[count] );
+  $("#needy-button").css( "background-color", colors[colorCount]);
+  $("html").css( "background-color", colors[colorCount]);
+  console.log();
 });
